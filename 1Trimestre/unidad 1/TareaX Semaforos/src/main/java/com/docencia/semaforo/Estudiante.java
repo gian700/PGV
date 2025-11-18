d8ad368a35f86de6b0a5b13736886f25b7270b86
Giancarlo Alejandro Trezza Riera <giancarloatr@gmail.com>
2025-11-18 15:08:44 +0000

    Add files via upload

---
+++ 1Trimestre/unidad 1/TareaX Semaforos/src/main/java/com/docencia/semaforo/Estudiante.java
@@ -0,0 +1,75 @@
+package com.docencia.semaforo;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author giancarlo
+ * @version 1.0.0
+ */
+public class Estudiante extends Thread {
+
+    private final String nombre;
+    private static final Semaphore semaforo = new Semaphore(4, true);
+
+    /**
+     * Contructor vacio
+     */
+    public Estudiante() {
+        this.nombre = "";
+    }
+
+    /**
+     * Constructor con el nombre del estudiante
+     *
+     * @param nombre nombre del estudiante
+     */
+    public Estudiante(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void run() {
+        try {
+            semaforo.acquire();
+            try {
+                System.out.println("El " + nombre + " ha comenzado a utilizar el equipo ");
+                Thread.sleep(ThreadLocalRandom.current().nextInt(3000, 5001));
+                System.out.println("El " + nombre + " ha finalizado con el equipo ");
+            } finally {
+                semaforo.release();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Thread estudiante1 = new Thread(new Estudiante("Estudiante 1"));
+        Thread estudiante2 = new Thread(new Estudiante("Estudiante 2"));
+        Thread estudiante3 = new Thread(new Estudiante("Estudiante 3"));
+        Thread estudiante4 = new Thread(new Estudiante("Estudiante 4"));
+        Thread estudiante5 = new Thread(new Estudiante("Estudiante 5"));
+        Thread estudiante6 = new Thread(new Estudiante("Estudiante 6"));
+
+        estudiante1.start();
+        estudiante2.start();
+        estudiante3.start();
+        estudiante4.start();
+        estudiante5.start();
+        estudiante6.start();
+
+        try {
+            estudiante1.join();
+            estudiante2.join();
+            estudiante3.join();
+            estudiante4.join();
+            estudiante5.join();
+            estudiante6.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
---
+++ 1Trimestre/unidad 1/TareaX Semaforos/src/main/java/com/docencia/semaforo/SemaforoCaseroMejorado.java
@@ -0,0 +1,112 @@
+package com.docencia.semaforo;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author giancarlo
+ * @version 1.0.0
+ */
+public class SemaforoCaseroMejorado implements Runnable {
+
+    private final String color;
+    private static final AtomicBoolean running = new AtomicBoolean(false);
+    private static final Semaphore semaphore = new Semaphore(1, true);
+    private static String turno = "Rojo";
+
+    /**
+     * Constructor vacio
+     */
+    public SemaforoCaseroMejorado() {
+        this.color = "";
+    }
+
+    /**
+     * Constructor con el color del semaforo
+     *
+     * @param color color del semaforo
+     */
+    public SemaforoCaseroMejorado(String color) {
+        this.color = color;
+    }
+
+        /**
+     * Metodo que gestiona el tiempo segun el color y actualiza el turno
+     * @return tiempo en milisegundos
+     */
+    private int gestionarColor() {
+        int tiempo = 0;
+
+        switch (color) {
+            case "Rojo":
+                tiempo = 3000;
+                turno = "Verde";
+                break;
+
+            case "Verde":
+                tiempo = 3000;
+                turno = "Ambar";
+                break;
+
+            case "Ambar":
+                tiempo = 1000;
+                turno = "Rojo";
+                break;
+        }
+
+        return tiempo;
+    }
+
+    @Override
+    public void run() {
+        while (!running.get()) {
+            try {
+                semaphore.acquire();
+
+                if (!turno.equals(color)) {
+                    continue;
+                }
+
+                System.out.println("Color actual: " + color);
+
+                int tiempo = gestionarColor();
+                Thread.sleep(tiempo);
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                semaphore.release();
+            }
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        Thread rojo = new Thread(new SemaforoCasero("Rojo"));
+        Thread verde = new Thread(new SemaforoCasero("Verde"));
+        Thread ambar = new Thread(new SemaforoCasero("Ambar"));
+
+        rojo.start();
+        verde.start();
+        ambar.start();
+        
+
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        running.set(true);
+
+
+        try {
+            rojo.join();
+            verde.join();
+            ambar.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
